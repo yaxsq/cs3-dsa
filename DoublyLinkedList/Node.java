00fd25099d474df66f8d180ab88396562becd886
Shazain <112701772+yaxsq@users.noreply.github.com>
2023-09-18 01:51:09 +0500

    Add files via upload

---
+++ DoublyLinkedList/Node.java
@@ -0,0 +1,13 @@
+public class Node <T> {
+
+    T data;
+    Node<T> prev;
+    Node<T> next;
+
+    Node(T data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+
+}
